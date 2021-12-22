2a5466eba93f503885a41b68dbdc8de918e9a24a
quidwhiffle <bobeishaisag@gmail.com>
2021-12-22 03:39:34 -0500

    fixed it so it no longer adds duplicates

--- listentothis.py
+++ listentothis.py
@@ -18,8 +18,10 @@ playlists = sp.current_user_playlists()
 for i in playlists['items']:
     if i['name'] == "Listen to This 2.0":
         PLAYLIST_ID =  i['id']
+with open('ids.txt') as f:
+    inplaylist = [line.rstrip() for line in f]
 
-inplaylist = []
+
 def get_playlist_tracks(playlist_id):
     results = sp.playlist_tracks(playlist_id)
     tracks = results['items']
@@ -28,80 +30,72 @@ def get_playlist_tracks(playlist_id):
         tracks.extend(results['items'])
     return tracks
 
-for track in get_playlist_tracks(PLAYLIST_ID):
-    trackid  = track['track']['id']
-    inplaylist.append(trackid)
+# for track in get_playlist_tracks(PLAYLIST_ID):
+#     trackid  = track['track']['id']
+#     inplaylist.append(trackid)
 
-with open('ids.txt', 'a') as f:
-    for track in inplaylist:
-        f.write(track)
-        f.write('\n')
-# reddit = praw.Reddit(
-#     client_id = os.getenv('REDDIT_CLIENT_ID'),
-#     client_secret = os.getenv('REDDIT_CLIENT_SECRET'),
-#     user_agent = os.getenv('USER_AGENT'),
-# )
-# def addSongs():
-#     subreddit = reddit.subreddit("listentothis")
-#     track_ids = []
-#     song_titles_toSearch = []
-#     similarity = 70
-#     anotherlist = []
-#     missed = []
+# with open('ids.txt', 'a') as f:
+#     for track in inplaylist:
+#         f.write(track)
+#         f.write('\n')
 
-#     for submission in subreddit.top("day"):
-#         x = submission.title.index('[')
-#         song = submission.title[:x].lower()
-#         if song :
-#             song_titles_toSearch.append(song)
+reddit = praw.Reddit(
+    client_id = os.getenv('REDDIT_CLIENT_ID'),
+    client_secret = os.getenv('REDDIT_CLIENT_SECRET'),
+    user_agent = os.getenv('USER_AGENT'),
+)
+def addSongs():
+    subreddit = reddit.subreddit("listentothis")
+    
+    song_titles_toSearch = []
+    similarity = 70
+    anotherlist = []
+    missed = []
 
-
-
+    for submission in subreddit.top("day"):
+        x = submission.title.index('[')
+        song = submission.title[:x].lower()
+        if song :
+            song_titles_toSearch.append(song)
         
-#     for title in song_titles_toSearch:
+    for title in song_titles_toSearch:
 
-#         if "--" in title:
-#             title = title.split('--')
-#         elif "- -" in title:
-#             title = title.split('- -')
+        if "--" in title:
+            title = title.split('--')
+        elif "- -" in title:
+            title = title.split('- -')
         
-#         elif '—' in title:
-#             title = title.split('—')
-#         elif '–' in title:
-#             title = title.split('–')
-#         elif '-' in title:
-#             title = title.split('-')
+        elif '—' in title:
+            title = title.split('—')
+        elif '–' in title:
+            title = title.split('–')
+        elif '-' in title:
+            title = title.split('-')
     
 
-#         results = sp.search(q="track:" + title[1] + title[0], type="track")
-#         if len(results['tracks']['items']) != 0:
-#             for result in results['tracks']['items']:
-#                 resultName  = result['name'].lower()
-#                 resultArtists =  result['artists'][0]['name'].lower()
-#                 titleArtist = resultName + '--' + resultArtists
+        results = sp.search(q="track:" + title[1] + title[0], type="track")
+        if len(results['tracks']['items']) != 0:
+            for result in results['tracks']['items']:
+                resultName  = result['name'].lower()
+                resultArtists =  result['artists'][0]['name'].lower()
+                titleArtist = resultName + '--' + resultArtists
 
-#                 if (fuzz.partial_ratio(resultName, title[1]) >= similarity) and (fuzz.partial_ratio(resultArtists, title[0])) >= similarity:
-#                     if not result['id'] in anotherlist:
-#                         anotherlist.append(result['id'])
-#                         break
-#                 else:
-#                     print(f"the score for title similarity between {resultName} and {title[1]} was {fuzz.partial_ratio(resultName, title[1])} and the score for artist similarity between {resultArtists} and {title[0]} was {fuzz.partial_ratio(resultArtists, title[0])}")
-#                     missed.append(title)
-#         else:
-#             print(f"{title[1]} {title[0]} could not be found")
-
-
-                
-        
-
-#     #print(len(song_titles_toSearch))
-# # print(f"there were {len(song_titles_toSearch)} songs in the original list, and {len(anotherlist)} songs in the found ones, so {len(song_titles_toSearch) - len(anotherlist)} songs were missed")
-#     sp.playlist_add_items(playlist_id=PLAYLIST_ID, items=anotherlist)
-#     print(len(anotherlist))
+                if (fuzz.partial_ratio(resultName, title[1]) >= similarity) and (fuzz.partial_ratio(resultArtists, title[0])) >= similarity:
+                    if not result['id'] in anotherlist and not result['id'] in inplaylist:
+                        anotherlist.append(result['id'])
+                     
+                        break
+                else:
+                    print(f"the score for title similarity between {resultName} and {title[1]} was {fuzz.partial_ratio(resultName, title[1])} and the score for artist similarity between {resultArtists} and {title[0]} was {fuzz.partial_ratio(resultArtists, title[0])}")
+                    missed.append(title)
+        else:
+            print(f"{title[1]} {title[0]} could not be found")
 
 
-# addSongs()
-# addSongs()
+    sp.playlist_add_items(playlist_id=PLAYLIST_ID, items=anotherlist)
+    with open('ids.txt', 'a') as f:
+        for line in anotherlist:
+            f.write(line)
+            f.write('\n')
 
-#i should use the aforementioned method to add the ids of all the songs in the playlist to a like text file or some permantent storage, and befoer adding the song ot the plauylsit, ti check whether or not the song is in the list of ids already in teh playlsit, and if not, only hten it appensds
-# i already have a list of all the ids in the playlist so each time i run the program i'll check if the id is in the file, and if no ti append it to the list of stuff to add to the playlist. after the stuff is appended, then i write the list to the list of ids in the playlist
+addSongs()
